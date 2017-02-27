ded9b6a0e7ed3459fa99afeeb913ef198f4f7ea1
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-27 12:05:16 +0100

    init

---
+++ item.controller.js
@@ -0,0 +1,13 @@
+// CREA UN CONTROLLER PER L'OGGETTO
+
+
+
+// CREA UN OGGETTO ARMA E RIEMPILO CON I SEGUENTI DATI
+/*
+ * nome: "Ascia bipenne",
+ * descrizione: "L’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
+ * slots : "3",
+ * image: "http://khazalidgrungron.altervista.org/axes/05.png"
+ */
+
+//
