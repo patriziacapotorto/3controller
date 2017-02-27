e303739118a87ae64d4164421cf4b574676122b4
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-27 12:27:34 +0100

    soluzione

--- item.controller.js
+++ item.controller.js
@@ -1,13 +1,28 @@
 // CREA UN CONTROLLER PER L'OGGETTO
+angular.module('app').controller('ItemCtrl', function ($scope) {
+
+	$scope.titolo = "Armi";
+
+	// CREA UN OGGETTO ARMA E RIEMPILO CON I SEGUENTI DATI
+	/*
+	 * nome: "Ascia bipenne",
+	 * descrizione: "L’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
+	 * slots : "3",
+	 * image: "http://khazalidgrungron.altervista.org/axes/05.png"
+	 */
+
+	$scope.arma = {
+		nome: "Ascia bipenne",
+		descrizione: "L’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
+		slots: "3",
+		image: "http://khazalidgrungron.altervista.org/axes/05.png"
+	};
+
+	//CREARE UN METODO PER MOSTRARE LA DESCRIZIONE DELL'ARMA
+	$scope.mostraDescrizione = function () {
+		$scope.descrizione = $scope.arma.descrizione;
+		console.log("here");
+	};
 
 
-
-// CREA UN OGGETTO ARMA E RIEMPILO CON I SEGUENTI DATI
-/*
- * nome: "Ascia bipenne",
- * descrizione: "L’ascia bipenne è una scure a due lame, simbolo del potere minoico.",
- * slots : "3",
- * image: "http://khazalidgrungron.altervista.org/axes/05.png"
- */
-
-//
+})
